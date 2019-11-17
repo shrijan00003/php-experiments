fa570e7914b49636e591fbfbc5f25dc559dc16b8
Shrijan Tripathi <shrijantripathi@Shrijans-MacBook-Pro.local>
2019-11-17 22:41:10 +0545

    php constant to include vs required is completed

---
+++ embedded.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello world !!!" ?>
+    <?php for($i=0; $i<10; $i++){?>
+        <h1>hello world</h1>
+    <?php } ?>
+</body>
+</html>
+  
--- variables.php
+++ variables.php
@@ -1,3 +1,8 @@
 <?php 
+/**
+ * @access public
+ * @param $amount
+ * @return $taxAmount
+ */
     echo "hello world !!"
 ?>
